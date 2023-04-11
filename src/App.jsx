e654ae4b2e0a1e73f97b9acc09d646c24454a05e
Lenovo <raintruwalker@gmail.com>
2023-04-11 17:38:56 +0300

    Add Routing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import styled from "styled-components";
+import Main from "./pages/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div>Привет</div>
-  )
+const Wrapper = styled.div`
+  width: 100%;
+`
+
+const App = () => {
+    return (
+        <Wrapper>
+            <Router>
+                <Header/>
+                <Routes>
+                    <Route exact path="/" element={<Main/>}/>
+                </Routes>
+                <Footer/>
+            </Router>
+        </Wrapper>
+    )
 }
 
 export default App
