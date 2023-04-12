36d2dac28309f234cc32ff03e5d559873bcffc28
Lenovo <raintruwalker@gmail.com>
2023-04-12 04:51:59 +0300

    Design Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,50 @@
 import styled from "styled-components";
+import logo from "../assets/images/logo.png";
+import {useNavigate} from "react-router-dom";
 
 const Wrapper = styled.div`
   width: 100%;
 `
 
 const Container = styled.div`
-  width: 100%;
-  padding: 0;
+  width: calc(100% - 4vw);
+  padding: 1vw 2vw;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
-  background-color: firebrick;
+  justify-content: center;
+  gap: 1vw;
+`
+
+const Logo = styled.img`
+  width: 9vw;
+  height: 4.5vw;
+  cursor: pointer;
+
+  &:hover {
+    opacity: .7;
+  }
+`
+
+const Copyright = styled.span`
+  font-weight: 900;
+`
+
+const Description = styled.span`
+  font-size: 1rem;
 `
 
 const Footer = () => {
+    const navigate = useNavigate()
+
     return (
         <Wrapper>
-            <Container>Футер</Container>
+            <Container>
+                <Logo src={logo} alt="Logo Image" onClick={() => navigate("/")}/>
+                <Copyright>© 2023-2023 Migliore</Copyright>
+                <Description>Все названия игр, компаний, торговых марок, логотипы и игровые материалы являются
+                    собственностью соответствующих владельцев.</Description>
+            </Container>
         </Wrapper>
     )
 }
